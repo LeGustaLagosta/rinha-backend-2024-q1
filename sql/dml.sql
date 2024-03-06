8a5969eb548829e720b642593e2e7b8be80b40d7
LeGustaLagosta <legustalagosta@pop-os.localdomain>
2024-03-05 22:23:59 -0300

    iniciando uso do postgresql

---
+++ sql/dml.sql
@@ -0,0 +1,5 @@
+INSERT INTO clientes (limite, saldo) VALUES (100000.0, 0.0);
+INSERT INTO clientes (limite, saldo) VALUES (80000.0, 0.0);
+INSERT INTO clientes (limite, saldo) VALUES (1000000.0, 0.0);
+INSERT INTO clientes (limite, saldo) VALUES (10000000.0, 0.0);
+INSERT INTO clientes (limite, saldo) VALUES (500000.0, 0.0);
